6f6b11b1c29e097157c66397d5089dcc9c24d3fa
Leo03-coder <wicked.brain23@gmail.com>
2026-02-25 17:54:31 +0100

    Fixed plausibleRequest math

--- vcuNode/plausibility.cpp
+++ vcuNode/plausibility.cpp
@@ -3,7 +3,7 @@
 void pedalValue(int pot1, int pot2, int plausibleRequest, bool isPlausible){
 
   float senosrValueDifference = ((float)abs(pot1 - pot2) / 1024) * 100.0;
-  plausibleRequest = ((int)(senosrValueDifference * 100));
+  plausibleRequest = ((int)(senosrValueDifference * 10));
 
   static int sensorTimer = 0;
 
